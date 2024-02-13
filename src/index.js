3378926cce7d78ac2f6f89b6fedbd131fe048f85
Kelsey OConnell <kelseyoconnell2217@gmail.com>
2024-02-12 20:48:45 -0500

    Complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
-  
-}
+    const inputForm = document.querySelector('form')
+    inputForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const input = event.target.querySelector("#searchByID").value
+        fetch(`http://localhost:3000/movies/${input}`)
+            .then(resp => resp.json())
+            .then(data => {
+                let title = document.querySelector("section#movieDetails h4");
+                let summary = document.querySelector("section#movieDetails p")
+                title.innerText = data.title;
+                summary.innerText = data.summary})
+        })
+    }
+
+
+
+
+
+
 
 document.addEventListener('DOMContentLoaded', init);
